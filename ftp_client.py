9ceed69ed0df0d5e07142e88313f81578239fef0
CheGevarAa <58992026+CheGevarAa@users.noreply.github.com>
2021-05-13 10:55:20 +0300

    ftp_server с документацией

--- ftp_client.py
+++ ftp_client.py
@@ -1,22 +1,22 @@
-import socket
-HOST = 'localhost'
-PORT = 8000
+import socket #импорт библиотеки для работы
+HOST = 'localhost' #адрес хоста
+PORT = 8000 #подключаемый порт
 
 requests = ['GET /connect/UserOne/12345', 'GET /pwd/', 'GET /mkdir/myfolder', 'GET /ls/', 'GET /rename/myfolder//documents',
-            'GET /rmdir/myfolder', 'GET /rmfile/', 'GET /toclient/documents/text.txt', 'GET /disconnect/']
-
+            'GET /rmdir/myfolder', 'GET /rmfile/', 'GET /toclient/documents/text.txt', 'GET /disconnect/'] #список запросов для обработки
+#циклом проверяем работу по всем запросам сразу
 for request in requests:
-    sock = socket.socket()
+    sock = socket.socket() #подключение к сокету
     try:
-        sock.connect((HOST, PORT))
-    except Exception as e:
-        print(e)
-    print(request)
-    sock.send(request.encode())
-    response = sock.recv(8192).decode()
-    if response == 'error':
+        sock.connect((HOST, PORT)) #в начаое проверка возможности подключения к хосту и порту
+    except Exception as e: 
+        print(e) #если невозможно, вывод ошибки
+    print(request) #вывод запроса который был отправлен
+    sock.send(request.encode()) #перевод текстового запроса в поток байт
+    response = sock.recv(8192).decode() #обратный процесс для получения ответа
+    if response == 'error':#проверка наличия ошибки и ее вывод, если она обнаружена, если нет, вывод ответа 
         print('Error in ' + request)
     else:
         print(response)
 
-    sock.close()
+    sock.close()#закрытие подключения
